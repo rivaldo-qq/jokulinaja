a768cfda7684d22db59418099d461ca6b0b83529
rivaldo-qq <70063901+rivaldo-qq@users.noreply.github.com>
2024-02-21 17:42:21 +0700

    Update CategoriesController.php

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Category;
-use App\Models\product;
+use App\Models\Product;
 
 
 class CategoriesController extends Controller
